94b63a087a4b4499e3a3aba560406d20c8442393
Tarek <elhaj102@mail.chapman.edu>
2022-05-19 14:45:26 -0700

    updated the favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,30 @@
 // _app.js
+import { Fragment } from 'react';
 import { supabase } from '../lib/supabase';
 import { Auth } from '@supabase/ui';
 import { UserProvider } from '../context/UserContext';
 import { StocksProvider } from '../context/StocksContext';
 import { SettingsProvider } from '../context/SettingContext';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Auth.UserContextProvider supabaseClient={supabase}>
-			<SettingsProvider>
-				<UserProvider>
-					<StocksProvider>
-						<Component {...pageProps} />
-					</StocksProvider>
-				</UserProvider>
-			</SettingsProvider>
-		</Auth.UserContextProvider>
+		<Fragment>
+			<Head>
+				<title>Stock Finder</title>
+				<link rel="shortcut icon" href="/images/stock.png" />
+			</Head>
+			<Auth.UserContextProvider supabaseClient={supabase}>
+				<SettingsProvider>
+					<UserProvider>
+						<StocksProvider>
+							<Component {...pageProps} />
+						</StocksProvider>
+					</UserProvider>
+				</SettingsProvider>
+			</Auth.UserContextProvider>
+		</Fragment>
 	);
 }
 
